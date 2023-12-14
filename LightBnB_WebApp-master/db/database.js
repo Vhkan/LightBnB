16b4b599dac2ec40c5218e254e5abba6b96bcbcd
Vhkan <vhkan@yahoo.com>
2023-12-14 00:54:50 -0700

    Add refactored addProperty function

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -4,6 +4,7 @@ const users = require("./json/users.json");
 
 //Setting up the pg to set up the connection with lighbnb DB
 const { Pool } = require('pg');
+const { prototype } = require("pg/lib/client");
 
 const pool = new Pool({
   user: 'vagrant',
@@ -252,12 +253,53 @@ const getAllProperties = async (options, limit = 10) => {
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
-const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+// const addProperty = function(property) {
+//   const propertyId = Object.keys(properties).length + 1;
+//   property.id = propertyId;
+//   properties[propertyId] = property;
+//   return Promise.resolve(property);
+// };
+
+// Refactored addProperty function
+const addProperty = async (property) => {
+  try {
+    const propertyObj = [
+      property.owner_id,
+      property.title,
+      property.description,
+      property.thumbnail_photo_url,
+      property.cover_photo_url,
+      property.cost_per_night,
+      property.street,
+      property.city,
+      property.province,
+      property.post_code,
+      property.country,
+      property.parking_spaces,
+      property.number_of_bathrooms,
+      property.number_of_bedrooms
+    ];
+    
+    const query = 'INSERT INTO properties \
+    (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, \
+    street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms) \
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *;';
+
+    //adding RETURNING *; to the end of the query to return the saved property
+
+    const result = await pool.query(query, propertyObj);
+
+    if (result.rows.length > 0) {
+      return result.rows[0];
+    } else {
+      return null;
+    }
+  } catch (err) {
+    console.log("Add property error is:", err);
+    throw err;
+  }
 };
+
 
 module.exports = {
   getUserWithEmail,
